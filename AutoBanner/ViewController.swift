eb3098c108cb6a2a0cf28a82ae94ac4c75484f40
hililyy <joun406@gmail.com>
2023-11-14 17:19:50 +0900

    [INIT] 프로젝트 생성

---
+++ AutoBanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AutoBanner
+//
+//  Created by 강조은 on 2023/11/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
